4e8e1d71c23437de0fa72f7e80456b7f37633759
Brendan Berg <brendan@plusminusfive.com>
2015-08-23 15:19:35 -0400

    clean up ell.js

--- src/ell.js
+++ src/ell.js
@@ -76,53 +76,8 @@ function eval(cmd, context, filename, callback) {
 	var command = str + cmd.replace(/^\((.*)\)$/, '$1').replace(/\n$/, '');
 	var ast, result;
 
-	// Process the command before attempting to parse. If the delimiters
-	// are not balanced, don't even try parsing; just buffer the command,
-	// set the prompt and return.
-	/*
-	var opPattern = /<=|>=|<-|->|=>|~>|<~/;
-	var delimPattern = /[^\[{\(<>\)}\]]+/g;
-	var arrowPattern = /\)>([{\(])/g;
-	var cmd_delims = command.replace(delimPattern, '').replace(arrowPattern, ')$1');
-	var openers = {'[': ']', '{': '}', '(': ')', '<': '>'};
-	var closers = {']': '[', '}': '{', ')': '(', '>': '<'};
-	var stack = [];
-	var delim;
-
-	// We extract only the delimiters from the command and iterate through
-	// building a stack.
-	for (var i = 0, len = cmd_delims.length; i < len; i++) {
-		if (cmd_delims[i] in openers) {
-			stack.push(cmd_delims[i]);
-		} else if (closers[cmd_delims[i]] === stack[stack.length - 1]) {
-			stack.pop();
-		} else {
-			str = '';
-			rep.setPrompt('>> ');
-			result = (
-				"Syntax Error: Unbalanced delimiters. Found '" + cmd_delims[i] +
-				"' but expected '" + openers[stack[stack.length - 1]] + "'."
-			);
-			callback(null, fmt.stylize(result, 'error'));
-			return;
-		}
-	}
-	*/
-
-/*	if (stack.length > 0) {
-		//console.log('stack', stack.join(''));
-		delim = fmt.stylize(stack[stack.length - 1], 'comment');
-		rep.setPrompt(' ' + delim + '   ' + Array(stack.length).join('  '));
-		str = command + '\n';
-		callback(null, undefined);
-		return;
-	} else {
-		str = '';
-		rep.setPrompt('>> ');
-	}*/
-
 	if (command.trim() === '') {
-		//callback(null, null);
+		//callback(null, undefined);
 		rep.displayPrompt();
 		return;
 	}
@@ -142,14 +97,10 @@ function eval(cmd, context, filename, callback) {
 		result = fmt.stylize(e.toString(), 'error') + '\n';
 		
 		if (e.line && e.column) {
+			// Parser errors come with line, column, offset, expected,
+			// and found properties.
 			var pointer = Array(e.column).join(' ') + fmt.stylize('^', 'string');
 			result = '   ' + pointer + '\n' + result;
-
-			result += fmt.stylize('    Line:     ' + e.line, 'string') + '\n';
-			result += fmt.stylize('    Column:   ' + e.column, 'string') + '\n';
-			result += fmt.stylize('    Offset:   ' + e.offset, 'string') + '\n';
-			result += fmt.stylize('    Expected: ' + JSON.stringify(e.expected), 'string') + '\n';
-			result += fmt.stylize('    Found:    ' + e.found, 'string') + '\n';
 		} else {
 			result += fmt.stylize(e.stack.replace(/^[^\n]+\n/, ''), 'string');
 		}
@@ -170,4 +121,3 @@ function eval(cmd, context, filename, callback) {
 		callback(null, result);
 	}
 }
-
